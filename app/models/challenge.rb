a024de7f46959973aa5764a0df05a7fff0ec974e
workjortiz <Carnash.25z>
2024-12-09 10:33:08 -0600

    Add Form validations

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,2 +1,6 @@
 class Challenge < ApplicationRecord
+
+    validates :name, presence: true, length: {minimum: 5, maximum: 10, message: "must be between 10 and 50 characters"}
+    validates :original_content, presence: true, length: {minimum: 20,maximun: 250, message: "must be between 20 and 250 characters"}
+
 end
