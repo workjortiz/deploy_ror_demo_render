2cfba8a1d5f0f02f7ae9a857794f576ed8e69070
workjortiz <Carnash.25z>
2024-12-24 09:42:17 -0600

    Remove spaces with CI-error Lint code for consistent style [try3]

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,6 +1,6 @@
 class Challenge < ApplicationRecord
-    validates :name, presence: true, length: { minimum:10, maximum:50, message:"must be between 10 and 50 characters" }
-    validates :original_content, presence: true, length: { minimum: 10, maximun:250, message:"must be between 20 and 250 characters" }
+    validates :name, presence: true, length: { minimum: 10, maximum: 50, message: "must be between 10 and 50 characters" }
+    validates :original_content, presence: true, length: { minimum: 10, maximun: 250, message: "must be between 20 and 250 characters" }
 
     def process_content
         error_parse_arr = Array.new
